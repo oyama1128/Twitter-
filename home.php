6c129f7a1e439124e5a53bde8d433710c900a821
oyama1128 <ten2808@icloud.com>
2021-05-28 20:49:22 +0900

    add home.php

---
+++ home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="ホーム画面です">
+    <link rel="icon" href="/Views/img/logo-twitterblue.svg">
+    <!--Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link href="/Views/css/style.css" rel="stylesheet">
+    <title>ホーム画面 / Twitterクローン</title>
+</head>
+<body>
+    <div class="container">
+        <div class="side">
+            <div class="side-inner">
+            <ul class="nav flex-column">
+                <li class="nav-item"><a href="home.php" class="nav-link"><img src="/Views/img/logo-twitterblue.svg" alt="" class="icon"></a></li>
+                <li class="nav-item"><a href="home.php" class="nav-link"><img src="/Views/img/icon-home.svg" alt=""></a></li>
+                <li class="nav-item"><a href="search.php" class="nav-link"><img src="/Views/img/icon-search.svg" alt=""></a></li>
+                <li class="nav-item"><a href="notification.php" class="nav-link"><img src="/Views/img/icon-notification.svg" alt=""></a></li>
+                <li class="nav-item"><a href="profile.php" class="nav-link"><img src="/Views/img/icon-profile.svg" alt=""></a></li>
+                <li class="nav-item"><a href="post.php" class="nav-link"><img src="/Views/img/icon-post-tweet-twitterblue.svg" alt="" class="post-tweet"></a></li>
+                <li class="nav-item my-icon"><img src="/Views/img_uploaded/user/sample-person.jpg" alt=""></li>
+                </ul>
+            </div>
+        </div>
+        <div class="main"></div>
+    </div>
+</body>
+</html>
